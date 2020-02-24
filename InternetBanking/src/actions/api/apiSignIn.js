71190a67d4e3de65ff04818f5cc15206397e101d
Tran Quang Cuong <cuongtq@s-wifi.vn>
2020-02-24 22:30:32 +0700

    admin@gmail.com / 123456789

--- InternetBanking/src/actions/api/apiSignIn.js
+++ InternetBanking/src/actions/api/apiSignIn.js
@@ -6,16 +6,22 @@ export function signIn(params, callback) {
     const email = encodeURIComponent(params.email.toLowerCase());
     const password = encodeURIComponent(params.password);
     const formData = `email=${email}&password=${password}`;
-    axios.post(helper.getApiUrl('login'), formData).then(function (response) {
-        // console.log("response",response)
-        if (response.status === 200) {
-            callback(null, response.data)
-        }
-        else callback(response, null)
-    }).catch(function (error) {
-        console.log("ERROR SigIn:", error)
-        callback(error, null)
-    })
+    var data ={
+        status: "success",
+        token : "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOlwvXC9sb2NhbGhvc3Q6ODAwMFwvYXBpXC91c2VyXC9sb2dpbiIsImlhdCI6MTU4MjU1Nzg4OCwiZXhwIjoxNTgyNTYxNDg4LCJuYmYiOjE1ODI1NTc4ODgsImp0aSI6IjlPdzNKSmRsdEFiZWpkbkkiLCJzdWIiOjEsInBydiI6Ijg3ZTBhZjFlZjlmZDE1ODEyZmRlYzk3MTUzYTE0ZTBiMDQ3NTQ2YWEifQ.pZGi4CP5buQ-Y9HBL7Q6heBJPOeeOhvE21tmigYjU9M"
+    }
+
+    callback(null, data);
+    // axios.post(helper.getApiUrl('login'), formData).then(function (response) {
+    //      console.log("response",response)
+    //     if (response.status === 200) {
+    //         callback(null, response.data)
+    //     }
+    //     else callback(response, null)
+    // }).catch(function (error) {
+    //     console.log("ERROR SigIn:", error)
+    //     callback(error, null)
+    // })
 }
 
 export function getProfile(params, callback) {
