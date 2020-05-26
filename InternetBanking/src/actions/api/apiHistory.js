011c54c623e55d6a8a6041b74befbce71bc5fd5b
cuongph1196 <phancuong1196@gmail.com>
2020-05-26 20:54:32 +0700

    upadate

--- InternetBanking/src/actions/api/apiHistory.js
+++ InternetBanking/src/actions/api/apiHistory.js
@@ -3,9 +3,9 @@ import * as helper from '../../modules/Helper.js'
 
 //lấy lịch sử giao dịch theo STK, Mã khách hàng
 export function getHistoryBySTK(params, callback) {
-    // console.log("getCustomerByID params: ", params)
-    axios.get(helper.getApiUrl('history') + params.number_payment).then(function (response) {
-        // console.log("response",response)
+    // console.log("getCustomerByID params: ", params);
+    axios.get(helper.getApiUrl('history') + params.number_payment + '?from=' + params.from + '&to=' + params.to).then(function (response) {
+         console.log("response_his",response);
         if (response.status === 201) {
             callback(null, response.data.data)
         }
